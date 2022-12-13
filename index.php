feea113ce89a5c8df7f1901e818c42443077855a
nembo93 <francesco.c193@libero.it>
2022-12-13 14:16:01 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+$testo="Ecco il testo";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <h1>PHP Badwords</h1>
+    <p>Questo è il paragrafo: <? echo $testo ?></p>
+    <p>Questa è la lunghezza del paragrafo: <? echo strlen($testo) ?></p>
+</body>
+</html>
