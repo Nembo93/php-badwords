07494c09c5fa73e6be10f4bbe57bab699dd9af85
nembo93 <francesco.c193@libero.it>
2022-12-13 15:03:14 +0100

    Dichiara variabile con testo, salva in una variabile valore di parametro get, salva in una nuova variabile testo modificato con str_replace, stampa in html valori richiesti

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
-$testo="Ecco il testo";
+$testo="Il mio nome è Francesco";
+$bad_word = $_GET["word"];
+$nuovo_testo = str_replace($bad_word, "***", $testo);
 ?>
 
 
@@ -15,5 +17,10 @@ $testo="Ecco il testo";
     <h1>PHP Badwords</h1>
     <p>Questo è il paragrafo: <? echo $testo ?></p>
     <p>Questa è la lunghezza del paragrafo: <? echo strlen($testo) ?></p>
+
+    <p>Questo è il paragrafo censurato: <? echo str_replace($bad_word, "***", $testo) ?></p>
+    <p>Questo è il paragrafo censurato (variabile): <? echo $nuovo_testo ?></p>
+    <p>Questa è la lunghezza del paragrafo censurato: <? echo strlen($nuovo_testo) ?></p>
+    <h2>Parola censurata: <? echo $bad_word ?></h2>
 </body>
 </html>
